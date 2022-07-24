4f0f272c67b12735742f75cfa337367c1284a755
Vincent Schmandt <vincentscode@gmail.com>
2022-07-25 00:06:17 +0200

    Initial commit

---
+++ bash/settings.bash
@@ -0,0 +1,8 @@
+# history file
+HISTSIZE=1048576
+HISTFILE="$HOME/.bash_history"
+SAVEHIST=$HISTSIZE
+shopt -s histappend
+
+# defaults
+export EDITOR=nano
---
+++ shell/external.sh
@@ -0,0 +1,8 @@
+# Cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+# Python startup file
+export PYTHONSTARTUP=$HOME/.pythonrc
+
+# Docker
+export DOCKER_SCAN_SUGGEST=false
